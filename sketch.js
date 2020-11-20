e3b0c80a69fee3bfcc47e54888824d028765c4e7
shishir3611 <shishir.vaddadi@gmail.com>
2020-11-20 18:51:24 +0530

    initial commit

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
@@ -28,14 +29,15 @@ function setup(){
     box4 = new Box(920,240,70,70);
     pig3 = new Pig(810, 220);
 
-    log3 =  new Log(810,180,300, PI/2);
+    log2 =  new Log(810,180,300, PI/2);
 
     box5 = new Box(810,160,70,70);
-    log4 = new Log(760,120,150, PI/7);
-    log5 = new Log(870,120,150, -PI/7);
+    log3 = new Log(760,120,150, PI/7);
+    log4 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
-
+    sling = new Slingshot(bird.body,{x:200,y:100})
+   
 }
 
 function draw(){
@@ -54,11 +56,19 @@ function draw(){
     box3.display();
     box4.display();
     pig3.display();
-    log3.display();
+    log2.display();
 
     box5.display();
+    log3.display();
     log4.display();
-    log5.display();
 
     bird.display();
+    sling.display();   
+}
+
+function mouseDragged(){
+    Matter.Body.setPosition(bird.body,{x:mouseX,y:mouseY})
+}
+function mouseReleased(){
+    sling.fly();
 }
